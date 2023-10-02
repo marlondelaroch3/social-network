daa5a5c9f11417349fd04698e3b0e524a7fa3bc0
Marlon De La Roche <marlondelaroch3@gmail.com>
2023-10-02 12:16:30 -0500

    fix:

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Button from '../../components/atoms/Button/Button'
 import { useAuth0 } from '@auth0/auth0-react';
 
+import styles from './Login.module.css'
+
 const Login = () => {
     const { loginWithRedirect } = useAuth0()
-    
+
     return (
-        <div>
+        <div className={styles.loginContainer}>
             <Button handleClick={() => loginWithRedirect()}>
                 Login o register
             </Button>
